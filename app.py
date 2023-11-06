b029a439a23a675359af90e8461dd7ceee28a8bd
sejoya104 <sejoya104@gmail.com>
2023-11-06 09:44:18 -0500

    Adding create_post_form.html

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.get('/view-post')
+@app.get('/view_post')
 def view_post():
-    return render_template('view-post.html')
+    return render_template('view_post.html')
+
+@app.get('/create_post_form')
+def view_post_form():
+    return render_template('create_post_form.html')
