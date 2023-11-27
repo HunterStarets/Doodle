05ad17301e993f0f261e6ee3c392f6ba4a5de51e
sejoya104 <sejoya104@gmail.com>
2023-11-27 11:37:11 -0500

    Updating User model

--- src/models.py
+++ src/models.py
@@ -1,3 +1,24 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'app_user'
+
+    user_id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    username = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    first_name = db.Column(db.String(255), nullable=False)
+    last_name = db.Column(db.String(255), nullable=False)
+
+    def __init__(self, email: str, username: str, password: str, first_name: str, last_name: str) -> None:
+        self.email = email
+        self.username = username
+        self.password = password
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def __repr__(self) -> str:
+        return f'User({self.user_id}, {self.email}, {self.username}, {self.password}, {self.first_name}, {self.last_name})'
+    
